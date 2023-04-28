e0b3b7860337ee314dd1f54aa22f844ff7654eaf
Ruxxo <ebukaogd1@gmail.com>
2023-04-28 15:55:22 +0100

    another

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 
-let js = 'Amazing';
-if (js === 'Amazing') alert('javascript is Fun!!');
+// let js = 'Amazing';
+// if (js === 'Amazing') alert('javascript is Fun!!');
 
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
 
-//Declaring first name 
-let firstName = 'Ebuka';
-console.log(firstName);
+// //Declaring first name 
+// let firstName = 'Ebuka';
+// console.log(firstName);
 
-//let this house be the first part
+// //let this house be the first part
 
-let myFirstJob = 'programmer';
-// let myCurrentJob = 'teacher';
+// let myFirstJob = 'programmer';
+// // let myCurrentJob = 'teacher';
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-console.log(typeof true);
-console.log(typeof 'jonas');
-console.log(typeof 23);
+// console.log(typeof true);
+// console.log(typeof 'jonas');
+// console.log(typeof 23);
+
+//Math Operators
+
+const futureYear = 2037;
+const russoAge = futureYear - 1995;
+const danielAge = futureYear - 2000;
+console.log(russoAge, danielAge);
+
+console.log(russoAge * 2, danielAge / 10, 2 ** 3);
+//2**3 means 2 to the power of 3, which is 2*2*2
+
+
+const firstName = 'Ebuka';
+const lastName = ' Ogidi ';
+console.log(firstName + lastName);
+
+
+//Assignment operators
+let x = 10 + 5 //15
+x += 10 // x = x + 10 = 25
+x *= 4 // x= x * 4 =100 
+x++; // x= x +1 
+x--; //x =x-1 
+x--;
+
+console.log(x);
+
+//Comparison operators; to produce boolean values.
+console.log(russoAge > danielAge); //>,<,>=,<=
+console.log(danielAge >= 18);
+console.log(russoAge <= 18);
+
