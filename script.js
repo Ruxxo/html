fe8ad95c631683bc5b8bc0e711820ab2eca08685
Ruxxo <ebukaogd1@gmail.com>
2023-04-28 15:06:44 +0100

    changes

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let js = 'Amazing';
+if (js === 'Amazing') alert('javascript is Fun!!');
+
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+//Declaring first name 
+let firstName = 'Ebuka';
+console.log(firstName);
+
+//let this house be the first part
+
+let myFirstJob = 'programmer';
+// let myCurrentJob = 'teacher';
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof 'jonas');
+console.log(typeof 23);
