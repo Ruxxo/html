d3b1b056485d46edda9b6838d54d2028ca0029da
Ruxxo <ebukaogd1@gmail.com>
2023-04-29 09:10:52 +0100

    new

--- script.js
+++ script.js
@@ -24,32 +24,48 @@
 
 //Math Operators
 
-const futureYear = 2037;
-const russoAge = futureYear - 1995;
-const danielAge = futureYear - 2000;
-console.log(russoAge, danielAge);
+// const futureYear = 2037;
+// const russoAge = futureYear - 1995;
+// const danielAge = futureYear - 2000;
+// console.log(russoAge, danielAge);
 
-console.log(russoAge * 2, danielAge / 10, 2 ** 3);
-//2**3 means 2 to the power of 3, which is 2*2*2
-
-
-const firstName = 'Ebuka';
-const lastName = ' Ogidi ';
-console.log(firstName + lastName);
+// console.log(russoAge * 2, danielAge / 10, 2 ** 3);
+// //2**3 means 2 to the power of 3, which is 2*2*2
 
 
-//Assignment operators
-let x = 10 + 5 //15
-x += 10 // x = x + 10 = 25
-x *= 4 // x= x * 4 =100 
-x++; // x= x +1 
-x--; //x =x-1 
-x--;
+// const firstName = 'Ebuka';
+// const lastName = ' Ogidi ';
+// console.log(firstName + lastName);
 
-console.log(x);
 
-//Comparison operators; to produce boolean values.
-console.log(russoAge > danielAge); //>,<,>=,<=
-console.log(danielAge >= 18);
-console.log(russoAge <= 18);
+// //Assignment operators
+// let x = 10 + 5 //15
+// x += 10 // x = x + 10 = 25
+// x *= 4 // x= x * 4 =100 
+// x++; // x= x +1 
+// x--; //x =x-1 
+// x--;
 
+// console.log(x);
+
+// //Comparison operators; to produce boolean values.
+// console.log(russoAge > danielAge); //>,<,>=,<=
+// console.log(danielAge >= 18);
+// console.log(russoAge <= 18);
+
+// const isFullAge = danielAge >= 18;
+
+//bmi = mass / height ** 2 = mass / (height * height)
+
+//Store mark and john.s heights in variables
+
+const johnHeight = 1.95;
+const markHeight = 1.69;
+const johnMass = 85;
+const markMass = 78;
+
+//Calculate both bmis using the formula above
+const johnBmi = 85 / (1.95 * 1.95);
+const markBmi = 78 / (1.69 * 1.69);
+
+//Creating a Boolean variable
