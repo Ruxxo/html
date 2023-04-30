86b7a586b084c845f372eadbc0c647697d1911aa
Ruxxo <ebukaogd1@gmail.com>
2023-04-30 20:09:33 +0100

    change

--- script.js
+++ script.js
@@ -291,3 +291,48 @@
 // }
 
 
+
+//Illustrate difference
+// const person = {
+//     name: "Steph",
+//     hair: "Brown"
+// }
+
+
+// const human = {
+//     id: "name"
+// }
+
+
+// console.log(person[human.id]); //returns Steph
+// console.log(person.human.id); //undefined
+
+//case statements
+
+
+
+const day = 'moday ';
+
+switch (day) {
+    case 'monday '://day ====monday
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('prepare Theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the Weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+
+}
